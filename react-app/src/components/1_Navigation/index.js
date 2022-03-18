3b9cc5d62e1f421e115b260af36e06d2ce217700
Mitch DePree <mdepree315@gmail.com>
2022-03-18 03:14:25 -0400

    styling

--- react-app/src/components/1_Navigation/index.js
+++ react-app/src/components/1_Navigation/index.js
@@ -1,10 +1,39 @@
+import {useEffect, useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
-import {NavDropdown} from '../Utils/dropdown';
+// todo ——————————————————————————————————————————————————————————————————————————————————
 import LogoutButton from '../../components/0_Session/LogoutButton';
-
 import './Navigation.css'
+// todo ——————————————————————————————————————————————————————————————————————————————————
+
+const NavDropdown = ({sessionUser}) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  
+  useEffect(()=> {
+    if (!showDropdown) return;
+    const closeDropdown = () => setShowDropdown(false);
+    document.addEventListener('click', closeDropdown)
+    document.removeEventListener('click', closeDropdown)
+  }, [showDropdown]);
+
+
+  return (<>
+    <img
+      onClick={() => showDropdown ? setShowDropdown(false) : setShowDropdown(true)}
+      className='nav-user-image'
+      src={sessionUser?.image_url} alt='user'
+    />
+
+    {showDropdown && (
+      <div className='dropdown-nav'>
+        <div>{sessionUser?.display_name}</div>
+        <input placeholder={sessionUser?.display_name}></input>
+        <input placeholder='User image url'></input>
+        <LogoutButton />
+      </div>
+    )}
+  </>)
+}
 
 const Navigation = () => {
   const sessionUser = useSelector(state => state?.session?.user);
@@ -16,7 +45,6 @@ const Navigation = () => {
       </div>
     
       <div id='mid-nav'>
-        <LogoutButton />
       </div>
     
       <div id='right-nav'>
