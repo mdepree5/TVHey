6ea5e772495090a14e5f9b95006a137785d3e8c1
Mitch DePree <mdepree315@gmail.com>
2022-03-22 00:33:46 -0400

    stable auth form with error handling

--- react-app/src/components/0_Session/AuthForm.js
+++ react-app/src/components/0_Session/AuthForm.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useLocation, Redirect } from 'react-router-dom';
 // todo ——————————————————————————————————————————————————————————————————————————————————
 import { login, signUp } from '../../store/session';
 import { FormErrors, FormInput } from '../Utils/forms';
 // todo ——————————————————————————————————————————————————————————————————————————————————
 const AuthForm = ({signup}) => {
   const dispatch = useDispatch();
+  const location = useLocation();
   const sessionUser = useSelector(state => state?.session?.user);
   
   const [username, setUsername] = useState('');
@@ -15,23 +16,20 @@ const AuthForm = ({signup}) => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errors, setErrors] = useState([]);
 
+  useEffect(() => {
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setErrors([]);
+  }, [location])
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (signup && password !== confirmPassword) {
-      setErrors(['Passwords must match']);
-      alert('Passwords must match', errors);
-      console.log('ERRORS', errors)
-      return;
-    }
 
     const user = signup ?
       await dispatch(signUp(username, email, password, confirmPassword)) : 
       await dispatch(login(email, password));
-
-    console.log(`%c user: ${user}`, `color:yellow`)
-    console.log(`%c user:`, `color:yellow`, user)
-    console.log(`%c user: ${user?.errors}`, `color:yellow`) //=> undefined
 
     if (user) setErrors(user);
     return;
@@ -49,21 +47,22 @@ const AuthForm = ({signup}) => {
   const passwordValidation = passwordRegex.exec(password) !== null;
   const confirmPasswordValidation = confirmPassword && confirmPassword === password;
   
-
+  const loginValidation = emailValidation && passwordValidation;
+  const signupValidation = usernameValidation && confirmPasswordValidation;
 
   return (
     <form onSubmit={handleSubmit}>
       {signup && 
-        <FormInput name='Username' show={signup} validation={usernameValidation} message='Provide a valid username' state={username} setState={setUsername}/>
-      }
+        <FormInput name='Username' show={signup} validation={usernameValidation} message='Provide a valid username' state={username} setState={setUsername}/>}
+      
       <FormInput name='Email' show={signup} validation={emailValidation} message='Provide a valid email' state={email} setState={setEmail}/>
-
       <FormInput type='password' show={signup} validation={passwordValidation} message='8 chars : Uppercase : lowercase : number : special' name='Password' state={password} setState={setPassword}/>
-
+      
       {signup && 
-        <FormInput required={true} show={signup} validation={confirmPasswordValidation} message='Passwords must match' type='password' name='Confirm Password' state={confirmPassword} setState={setConfirmPassword}/> 
-      }
-      <button type='submit' disabled={!usernameValidation && !emailValidation && !passwordValidation && !confirmPasswordValidation}>{signup ? 'Sign Up' : 'Log In'}</button>
+        <FormInput required={true} show={signup} validation={confirmPasswordValidation} message='Passwords must match' type='password' name='Confirm Password' state={confirmPassword} setState={setConfirmPassword}/> }
+      
+      <button className={loginValidation ? 'input-valid' : ''} type='submit' disabled={signup ? !(loginValidation && signupValidation): !loginValidation}>{signup ? 'Sign Up' : 'Log In'}</button>
+      
       <FormErrors errors={errors}/>
     </form>
   );
