26370fb4afb383e337b1f283d78adc0d47d9359e
Mitch DePree <mdepree315@gmail.com>
2022-04-07 11:42:07 -0400

    install frontend uuid, add websocket object client side instantiation to app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // todo ——————————————————————————————————————————————————————————————————————————————————
 import {UnAuthenticatedApp, AuthenticatedApp} from './components/2_Main';
 import {authenticate} from './store/session';
+// todo ——————————————————————————————————————————————————————————————————————————————————
+import { v4 as uuidv4 } from 'uuid';
 
 
 // todo ——————————————————————————————————————————————————————————————————————————————————
@@ -17,6 +19,40 @@ const App = () => {
       setPageLoaded(true);
     })()
   }, [dispatch]);
+
+  const websocket = useRef(null);
+
+  useEffect(() => {
+    if (!sessionUser) return;
+
+    console.log(`%c process.env.REACT_APP_WS_URL:`, `color:yellow`, process.env.REACT_APP_WS_URL)
+
+    const ws = new WebSocket(process.env.REACT_APP_WS_URL);
+
+    ws.onopen = e => {
+      console.log(`%c connection open:`, `color:yellow`, e)
+    }
+    
+    ws.onerror = e => console.error(`%c on error:`, `color:yellow`, e)
+    
+    ws.onclose = e => {
+      console.log(`%c connection closed:`, `color:yellow`, e)
+      websocket.current = null;
+    }
+
+    websocket.current = ws;
+
+    return () => {
+      if(websocket.current !== null) websocket.current.close();
+    }
+
+  }, [sessionUser])
+
+
+
+
+
+
 
   return pageLoaded && <>{sessionUser ? <AuthenticatedApp/> : <UnAuthenticatedApp/>}</>
 }
