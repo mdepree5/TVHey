52aa45d6e4c55b0479233243b1f549c5953266b8
Mitch <mdepree315@gmail.com>
2022-04-19 20:45:39 -0400

    create dm model and expand message model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user_model import User
 from .channel_model import Channel
 from .message_model import Message
+from .dm_model import DM
---
+++ app/models/dm_model.py
@@ -0,0 +1,27 @@
+from app.models.db import db
+from datetime import datetime, date
+
+class DM(db.Model):
+  __tablename__ = "dms"
+
+  id = db.Column(db.Integer, primary_key=True)
+  host_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+  title = db.Column(db.String(100), nullable=False)
+  topic = db.Column(db.String(255), nullable=False)
+  created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+  updated_at = db.Column(db.DateTime(), nullable=True, default=datetime.now())
+
+  users = db.relationship("User", back_populates="dms")
+  messages = db.relationship("Message", back_populates="dms", cascade="all, delete")
+
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "host_id": self.host_id,
+      "title": self.title,
+      "topic": self.topic,
+      "created_at": self.created_at,
+      "updated_at": self.updated_at,
+      'owner': self.users.display_name if self.users.display_name else self.users.username,
+    }
+
