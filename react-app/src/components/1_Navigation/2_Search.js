d9f2c6d6d4b87d847865feae14f60e85e4113de8
Mitch DePree <mdepree315@gmail.com>
2022-04-21 14:36:51 -0400

    relocate search pseudo

--- react-app/src/components/1_Navigation/2_Search.js
+++ react-app/src/components/1_Navigation/2_Search.js
@@ -41,63 +41,11 @@ const Search = () => {
         <div className='dropdown-nav' id='search'>
           <div className='col-list' >
             {searchInput && users?.map(user => 
-              <>
-                {/* 
-                  A NEW Modal, where you send a message. And part of the submission handling is that it 
-                    1. Reference the user's id and compare it with a host or recipient ID 
-                      a. OR: Create a slice of state that maps out all of the users that share a DM:
-                        It has Key:Val pairs that represent the user's id and the DM Id
-                          const kv state: {
-                            1: 5
-                            2: 7
-                            where "1" represents "user 1" and "5" represents 
-                          }
-                          The reason this is ideal is because a user can have MANY DMs
-                          This implementation is a space-for-time complexity tradeoff.
-                          We are creating a new object in state, which occupies space. 
-                          But the time tradeoff is we avoid having to iterate over and filter through ALL users each time we search/query.
-                          Instead, we can simply reference the slice of state that carries the relevant information we want: 
-                            i. the user Id and if the session user has dms with themselves
-                            ii. access to the dm Id itself, so we can route there if we need to (or create a new DM instance)
-                    2. If dm does not exist in state, create new dm
-                    3. If 
-
-                 */}
-                <div>
-                  Component with User's Display Name
-                  
-                  PERHAPS: Rather than use a modal...
-                  1. Create a custom div/component that has a [toggleClick, setToggleClick] = useState(false)
-                  2. {'toggleClick' && <div>
-                      Show the pseudo chat component form
-                    </div>}
-                  3. This way the thing is 'in-line' with each search-barred "user" component in the search dropdown
-
-                  1. OnClick - Open a modal to send a message
-                  2. Chat component message input form
-                    i. borrow all styling
-                    ii. modify handleSubmit to accommodate for below logic
-
-                  {selectedUsersDMs[user?.id] === undefined ? 
-                    <div>
-                      Open modal
-                      1. dispatch create new DM
-                      2. dispatch create new Message
-                      3. Route to newly created DM/newlyCreatedDM.id
-                      Return close modal
-                    </div>
-                  :
-                    <div>
-                      Open modal
-                      1. dispatch create new Message
-                      2. Route to existing DM/selectedUsersDMs[user?.id]
-                      Return close modal
-                    </div>
-                  }
-                </div>
-                <UserSearchComponent key={user.id} user={user} />
-                <NavLink to={`/dms/${user?.id}`} key={user?.id} className='channel-list-item' activeStyle={{backgroundColor:'#EC8642', color: 'white', display: 'unset'}} >{user?.privateStatus ? 'π' : '#'} {user?.display_name} {'X'}</NavLink>
-              </>
+              <div key={user?.id}>
+                
+                <UserSearchComponent key={user?.id} user={user} />
+                {/* <NavLink to={`/dms/${user?.id}`} key={user?.id} className='channel-list-item' activeStyle={{backgroundColor:'#EC8642', color: 'white', display: 'unset'}} >{user?.privateStatus ? 'π' : '#'} {user?.display_name} {'X'}</NavLink> */}
+              </div>
             )}
             {searchInput && channels?.map(channel => (
               <NavLink to={`/channels/${channel?.id}`} onClick={clickChannel} key={channel?.id} className='channel-list-item' activeStyle={{backgroundColor:'#EC8642', color: 'white', display: 'unset'}} >{channel?.privateStatus ? 'π' : '#'} {channel?.title}</NavLink>
@@ -122,35 +70,36 @@ const UserSearchComponent = ({user}) => {
   const selectedUsersDMs = dmstate?.selectedUsersDMs
 
 
-  const sendMessage = async(e) => {
-    e.preventDefault();
+  const sendMessage = () => alert('HEY')
+  // const sendMessage = async(e) => {
+  //   e.preventDefault();
 
-    if (selectedUsersDMs[user?.id] === undefined) {
-      let newDMId;
-      const dmData = {host_id: sessionUser?.id, recipient_id: user?.id}
-      socket.emit('create dm', dmData)
-      /* 
-        * this line should run from main.js: socket.on('dm to front', dm => dispatch(createDM(JSON.parse(dm))))
-        * then, state should update..
-        * 
-      */
-      socket.on('dm to front', dm => newDMId = JSON.parse(dm).id)
-      socket.emit('create dm message', {author_id: sessionUser?.id, dm_id: newDMId, content: input})
+  //   if (selectedUsersDMs[user?.id] === undefined) {
+  //     let newDMId;
+  //     const dmData = {host_id: sessionUser?.id, recipient_id: user?.id}
+  //     socket.emit('create dm', dmData)
+  //     /* 
+  //       * this line should run from main.js: socket.on('dm to front', dm => dispatch(createDM(JSON.parse(dm))))
+  //       * then, state should update..
+  //       * 
+  //     */
+  //     socket.on('dm to front', dm => newDMId = JSON.parse(dm).id)
+  //     socket.emit('create dm message', {author_id: sessionUser?.id, dm_id: newDMId, content: input})
 
-      history.push(`/dms/${newDMId}`)
-    } else {
-      const existingDMId = selectedUsersDMs[user?.id];
+  //     history.push(`/dms/${newDMId}`)
+  //   } else {
+  //     const existingDMId = selectedUsersDMs[user?.id];
 
-      console.log(`%c existingDMId:`, `color:yellow`, existingDMId)
+  //     console.log(`%c existingDMId:`, `color:yellow`, existingDMId)
 
-      socket.emit('create dm message', {author_id: sessionUser?.id, dm_id: existingDMId, content: input})
+  //     socket.emit('create dm message', {author_id: sessionUser?.id, dm_id: existingDMId, content: input})
   
-      history.push(`/dms/${existingDMId}`)
-    }
+  //     history.push(`/dms/${existingDMId}`)
+  //   }
 
-    setInput('');
-    return setToggleForm(false);
-  }
+  //   setInput('');
+  //   return setToggleForm(false);
+  // }
 
   const handleCancel = e => {
     e.preventDefault();
@@ -187,3 +136,63 @@ const UserSearchComponent = ({user}) => {
 
 
 export default Search;
+
+
+
+
+// todo ——————————————————————————————————————————————————————————————————————————————————
+// todo                               Pseudo for dm implementation
+// todo ——————————————————————————————————————————————————————————————————————————————————
+
+/* 
+A NEW Modal, where you send a message. And part of the submission handling is that it 
+  1. Reference the user's id and compare it with a host or recipient ID 
+    a. OR: Create a slice of state that maps out all of the users that share a DM:
+      It has Key:Val pairs that represent the user's id and the DM Id
+        const kv state: {
+          1: 5
+          2: 7
+          where "1" represents "user 1" and "5" represents 
+        }
+        The reason this is ideal is because a user can have MANY DMs
+        This implementation is a space-for-time complexity tradeoff.
+        We are creating a new object in state, which occupies space. 
+        But the time tradeoff is we avoid having to iterate over and filter through ALL users each time we search/query.
+        Instead, we can simply reference the slice of state that carries the relevant information we want: 
+          i. the user Id and if the session user has dms with themselves
+          ii. access to the dm Id itself, so we can route there if we need to (or create a new DM instance)
+  2. If dm does not exist in state, create new dm
+  3. If 
+<div>
+Component with User's Display Name
+
+PERHAPS: Rather than use a modal...
+1. Create a custom div/component that has a [toggleClick, setToggleClick] = useState(false)
+2. {'toggleClick' && <div>
+    Show the pseudo chat component form
+  </div>}
+3. This way the thing is 'in-line' with each search-barred "user" component in the search dropdown
+
+  1. OnClick - Open a modal to send a message
+  2. Chat component message input form
+    i. borrow all styling
+    ii. modify handleSubmit to accommodate for below logic
+
+  {selectedUsersDMs[user?.id] === undefined ? 
+    <div>
+      Open modal
+      1. dispatch create new DM
+      2. dispatch create new Message
+      3. Route to newly created DM/newlyCreatedDM.id
+      Return close modal
+    </div>
+  :
+    <div>
+      Open modal
+      1. dispatch create new Message
+      2. Route to existing DM/selectedUsersDMs[user?.id]
+      Return close modal
+    </div>
+  }
+</div> 
+*/
